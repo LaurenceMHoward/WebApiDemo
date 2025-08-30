7a1eebefad1290f6e56dd284c29b0187ef791746
Laurence Howard <lrnchwrd@gmail.com>
2025-08-30 04:31:43 +0100

    cleanup;

--- IntegrationTests/Reference/TestWebApplicationFactory.cs
+++ IntegrationTests/Reference/TestWebApplicationFactory.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace IntegrationTests.Reference;
@@ -10,6 +11,11 @@ using WebApiDemo.Dal.Records;
 
 public class TestWebApplicationFactory<TEntryPoint> : WebApplicationFactory<TEntryPoint> where TEntryPoint : class
 {
+    private static readonly ServiceProvider EfInMemoryProvider =
+        new ServiceCollection()
+            .AddEntityFrameworkInMemoryDatabase()
+            .BuildServiceProvider();
+
     private readonly CategoryRecord[] _categories =
     [
         new CategoryRecord() { Id = Guid.NewGuid(), Category = "TestA", SubCategory = "TestB" },
@@ -21,37 +27,46 @@ public class TestWebApplicationFactory<TEntryPoint> : WebApplicationFactory<TEnt
         } // this should save as IsDeleted = false
     ];
 
-    public int CategoryCount()
-    {
-        return _categories.Length;
-    }
-
-    public CategoryRecord GetFirstCategory()
-    {
-        return _categories[0];
-    }
+    public int CategoryCount() => _categories.Length;
+    public CategoryRecord GetFirstCategory() => _categories[0];
 
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
-        builder.ConfigureServices(services =>
+        builder.UseEnvironment("Testing");
+
+        builder.ConfigureTestServices(services =>
         {
-            // remove the original
-            ServiceDescriptor? descriptor =
-                services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<WebApiDemoDbContext>));
-            services.Remove(descriptor!);
+            // Remove app registrations
+            var registrations = services
+                .Where(d =>
+                    d.ServiceType == typeof(DbContextOptions<WebApiDemoDbContext>) ||
+                    d.ServiceType == typeof(IWebApiDemoDbContext) ||
+                    d.ServiceType == typeof(WebApiDemoDbContext))
+                .ToList();
+            foreach (var d in registrations)
+            {
+                services.Remove(d);
+            }
 
-            // add the new one
-            services.AddDbContext<WebApiDemoDbContext>(options => options.UseInMemoryDatabase("InMemoryDbForTesting"));
+            // Register InMemory DbContext using an isolated EF internal provider
+            var dbName = $"InMemoryDbForTesting-{Guid.NewGuid()}";
+            services.AddDbContext<WebApiDemoDbContext>(options =>
+            {
+                options.UseInMemoryDatabase(dbName);
+                options.UseQueryTrackingBehavior(QueryTrackingBehavior.TrackAll);
+                options.UseInternalServiceProvider(EfInMemoryProvider);
+            });
 
-            ServiceProvider sp = services.BuildServiceProvider();
+            // Map interface to concrete
+            services.AddScoped<IWebApiDemoDbContext>(sp => sp.GetRequiredService<WebApiDemoDbContext>());
 
-            using IServiceScope scope = sp.CreateScope();
-            IServiceProvider scopedService = scope.ServiceProvider;
-            WebApiDemoDbContext db = scopedService.GetRequiredService<WebApiDemoDbContext>();
+            // Seed
+            using var scope = services.BuildServiceProvider().CreateScope();
+            var db = scope.ServiceProvider.GetRequiredService<WebApiDemoDbContext>();
             db.Database.EnsureDeleted();
             db.Database.EnsureCreated();
             db.Set<CategoryRecord>().AddRange(_categories);
-            db.SaveChangesAsync();
+            db.SaveChanges();
             db.ChangeTracker.Clear();
         });
     }
